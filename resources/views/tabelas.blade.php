17c0ab738c2ca69b6e57ec32c35b1f05ab045f26
joaoeduardooliveira1977 <joaoeduardooliveira1977@gmail.com>
2026-05-08 23:59:25 -0300

    brad

--- resources/views/tabelas.blade.php
+++ resources/views/tabelas.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('page-title', 'Tabelas de Domínio')
-@section('breadcrumb')Administração <span class="sep">›</span> <span class="current">Tabelas de Domínio</span>@endsection
 @section('content')
     <livewire:tabelas-dominio />
 @endsection
